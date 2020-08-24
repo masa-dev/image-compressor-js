e14b68584ee4193a0ad19b9df43bfaba36dc6f1f
masa-dev <60791106+masa-dev@users.noreply.github.com>
2020-08-24 10:57:03 +0900

    変数によるメモリ消費量を削減，READMEファイルの更新
    一部変数の破壊メソッドにより，その変数が持つメモリを無くす．

--- js/config.js
+++ js/config.js
@@ -1,33 +1,60 @@
 const config = {
-    processingOnFileInput: true,    //ファイルインプット時に処理する
-    hideFileList: false, //ファイルリストを非表示にする
+    imageFileName: '',
+    folderName: 'pdf-image',
+
+    // Modal Window //
+    type: 'image/jpeg',
+    quality: 0.8,
+    scale: 1.5,
+    // Modal Window //
+
+    threeDigit: true,
+    noUnderscore: false,
 
     openModal: function () {
         const nameList = [
-            { id: 'processing-file-input', property: 'processingOnFileInput' },
-            { id: 'hide-file-list', property: 'hideFileList' }
-        ]
+            { property: 'type' },
+            { property: 'quality' },
+            { property: 'scale' },
+        ];
 
         for (let name of nameList) {
-            reflectsCheck(name.id, name.property);
-        }
-
-        function reflectsCheck(idName, propertyName) {
-            let element = document.getElementById(idName);
-            if (element.checked != config[propertyName]) {
-                element.parentNode.click();
+            let element = configElement[name.property];
+            if (element.value != this[name.property]) {
+                element.value = this[name.property];
             }
         }
     },
     closeModal: function () {
         document.getElementById('close-modal-btn').click();
     },
-    applyConfig: function () {
-        this.processingOnFileInput = document.getElementById('processing-file-input').checked;
-        this.hideFileList = document.getElementById('hide-file-list').checked;
-
-        fileInfo.seen = !this.hideFileList;
+    applyConfigOfModal: function () {
+        this.type = configElement.type.value;
+        this.quality = parseFloat(configElement.quality.value);
+        this.scale = parseFloat(configElement.scale.value);
 
         this.closeModal();
+    },
+    applyConfigOfProcess: function () {
+        if (configElement.folderName.value == '') {
+            this.folderName = 'pdf-image';
+        } else {
+            this.folderName = configElement.folderName.value;
+        }
+
+        this.imageFileName = configElement.imageFileName.value;
+
+        this.threeDigit = configElement.threeDigit.checked;
+        this.noUnderscore = configElement.noUnderscore.checked;
     }
-}
+};
+
+const configElement = {
+    imageFileName: document.getElementById('image-file-name'),
+    folderName: document.getElementById('folder-name'),
+    type: document.getElementById('image-file-type'),
+    quality: document.getElementById('file-quality'),
+    scale: document.getElementById('image-scale'),
+    threeDigit: document.getElementById('three-digit-number'),
+    noUnderscore: document.getElementById('no-underscore')
+};
